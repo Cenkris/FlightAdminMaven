7a407c21e10b1f443f728058d514b2fb2f146b3f
Traian Butaru <shwz23@yahoo.com>
2020-05-05 20:08:30 +0300

    - added login page;

--- src/main/java/Application/Main.java
+++ src/main/java/Application/Main.java
@@ -1,7 +1,9 @@
 package Application;
 
+import Application.View.LoginPage;
+
 public class Main {
     public static void main(String[] args) {
-
+        LoginPage loginPage = new LoginPage();
     }
 }
--- src/main/java/Application/View/LoginPage.java
+++ src/main/java/Application/View/LoginPage.java
@@ -4,4 +4,15 @@ import javax.swing.*;
 
 public class LoginPage extends JFrame {
 
+    public LoginPage(){
+        initDefaultValues();
+    }
+
+    private void initDefaultValues() {
+        setTitle("Login Page");
+        setSize(300,200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
