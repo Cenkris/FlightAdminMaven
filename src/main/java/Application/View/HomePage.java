56e9f8f40ffdd44155a58ef9f514a94c3d438c9c
Traian Butaru <shwz23@yahoo.com>
2020-05-08 21:17:26 +0300

    - added table button renderer;

--- src/main/java/Application/View/HomePage.java
+++ src/main/java/Application/View/HomePage.java
@@ -1,9 +1,15 @@
 package Application.View;
 
+import Application.Model.TableRendenderer;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Date;
@@ -12,24 +18,41 @@ public class HomePage extends JPanel {
     private JLabel clockLabel;
     private JPanel clockPanel;
     private JTable fligthTable;
-    private JButton addFlightButton;
-    private String[][] data = {{"Bucuresti", "Londra", "7:40", "10:25", "Luni, Marti, Joi", "1040"},
-            {"Londra", "Bucuresti", "13:05", "15:40", "Marti, Sambate", "965"}};
-    private String[] colNames = {"Sursa", "Destinatie", "Ora Plecare", "Ora Sosire", "Zile", "Pret"};
+    private JButton addFlightButton, deleteFlightButton;
+    private String[] colNames = {"", "Sursa", "Destinatie", "Ora Plecare", "Ora Sosire", "Zile", "Pret"};
+    private final int xAxisDimension = 600;
+    private TableCellRenderer tableCellRenderer;
+    private DefaultTableModel flightTableModel;
 
     public HomePage() {
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         initClock();
+        initDeleteFlightButton();
         initTablePanel();
         initAddFlightButton();
     }
 
+    private void initDeleteFlightButton() {
+        deleteFlightButton = new JButton();
+        deleteFlightButton.setBackground(Color.RED);
+
+        MouseAdapter deleteMouseAdapter = new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                JOptionPane.showMessageDialog(null,"GG");
+            }
+        };
+
+        deleteFlightButton.addMouseListener(deleteMouseAdapter);
+    }
+
     private void initAddFlightButton() {
+
+
         JPanel buttonPannel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         addFlightButton = new JButton("Add Flight");
-        addFlightButton.setPreferredSize(new Dimension(500, 30));
+        addFlightButton.setPreferredSize(new Dimension(xAxisDimension, 30));
         addFlightButton.addActionListener(event -> addRow());
-
         //add component
         buttonPannel.add(addFlightButton);
         add(buttonPannel);
@@ -40,26 +63,84 @@ public class HomePage extends JPanel {
     }
 
     private void initTablePanel() {
-        DefaultTableModel flightTableModel = new DefaultTableModel(){
-        };
+        Object[][] data = {{deleteFlightButton, "Bucuresti", "Londra", "7:40", "10:25", "Luni, Marti, Joi", "1040"},
+                {deleteFlightButton, "Londra", "Bucuresti", "13:05", "15:40", "Marti, Sambata", "965"}};
+
+        initTableModel();
         fligthTable = new JTable(flightTableModel);
-
-        //set column names
-        for(String colName : colNames){
-            flightTableModel.addColumn(colName);
-        }
-
-        //add rows
-        flightTableModel.addRow(data[0]);
-//        fligthTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        initTableCellRenderer();
+        setColumnNames(flightTableModel);
+//        addRowsToTable(data, flightTableModel);
+        setTableColumnPreferences();
 
         JScrollPane scrollPane = new JScrollPane(fligthTable);
         JScrollBar scrollBar = new JScrollBar();
         scrollPane.setVerticalScrollBar(scrollBar);
-        scrollPane.setPreferredSize(new Dimension(500,100));
+        scrollPane.setPreferredSize(new Dimension(xAxisDimension, 100));
 
         //add component
         add(scrollPane);
+    }
+
+    private void setTableColumnPreferences() {
+        TableColumnModel columnModel = fligthTable.getColumnModel();
+
+        //buttons Column
+        TableColumn buttonsColumn = columnModel.getColumn(0);
+        buttonsColumn.setPreferredWidth(10);
+
+        TableColumn sursaColumn = columnModel.getColumn(1);
+        sursaColumn.setPreferredWidth(100);
+
+        TableColumn destinatieColumn = columnModel.getColumn(2);
+        destinatieColumn.setPreferredWidth(100);
+
+        TableColumn oraPlecareColumn = columnModel.getColumn(3);
+        oraPlecareColumn.setPreferredWidth(80);
+
+        TableColumn oraSosireColumn = columnModel.getColumn(4);
+        oraSosireColumn.setPreferredWidth(80);
+
+        TableColumn zileColumn = columnModel.getColumn(5);
+        zileColumn.setPreferredWidth(180);
+
+        TableColumn pretColumn = columnModel.getColumn(6);
+        pretColumn.setPreferredWidth(70);
+
+    }
+
+    private void initTableCellRenderer() {
+        tableCellRenderer = fligthTable.getDefaultRenderer(JButton.class);
+        fligthTable.setDefaultRenderer(JButton.class, new TableRendenderer(tableCellRenderer));
+    }
+
+    private void initTableModel() {
+        flightTableModel = new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return column == 0;
+            }
+
+            @Override
+            public Class getColumnClass(int column) {
+                return getValueAt(0, column).getClass();
+            }
+
+        };
+
+
+    }
+
+    private void addRowsToTable(Object[][] data, DefaultTableModel flightTableModel) {
+        flightTableModel.addRow(data[0]);
+        flightTableModel.addRow(data[1]);
+    }
+
+    private void setColumnNames(DefaultTableModel flightTableModel) {
+        //set column names
+        for (Object colName : colNames) {
+            flightTableModel.addColumn(colName);
+        }
     }
 
     private void initClock() {
